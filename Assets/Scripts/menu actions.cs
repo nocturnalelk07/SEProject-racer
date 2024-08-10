b8aec45d175e4ed770257530aa01297dbebcc072
nocturnalelk07 <finnp@outlook.com>
2024-08-10 18:29:01 +0100

    bit worried ive broken something, turns out i didnt(?)

--- Assets/Scripts/menu actions.cs
+++ Assets/Scripts/menu actions.cs
@@ -1,20 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class menuActions : MonoBehaviour
 {
+    public int mainMenu = 0;
     public int firstScene = 1;
     public void QuitApplication()
     {
         Application.Quit();
-        //Debug.Log("quit game");
+        Debug.Log("quit game");
     }
 
     public void StartGame()
     {
-            SceneManager.LoadScene(firstScene);
+        SceneManager.LoadScene(firstScene);
+    }
+
+    public void ReturnToMenu()
+    {
+        SceneManager.LoadScene(mainMenu);
+    }
+
+    public void Reset()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
