5dcaac10c8f04a846d3a354c2bcc75b8f573f5ac
nocturnalelk07 <finnp@outlook.com>
2025-04-07 11:22:00 +0100

    lean tween animation graphs for envelopes

--- Assets/Scripts/Synthic/MusicController.cs
+++ Assets/Scripts/Synthic/MusicController.cs
@@ -12,7 +12,10 @@ public class MusicController : MonoBehaviour
     [SerializeField] private double modulatorDepth = 0.2;
     [SerializeField] private bool volume;
     [SerializeField] private bool pitch;
+    [SerializeField] private AnimationCurve graph;
 
+    private float graphTime = 0;
+    [SerializeField] float maxGraphTime = 1.5f;
     private float LFO_index;
 
     //this will set the volume of the wave being played
@@ -30,6 +33,12 @@ public class MusicController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        audioSource.volume = graph.Evaluate(graphTime);
+        graphTime += Time.fixedDeltaTime;
+        if (graphTime >= maxGraphTime) 
+        {
+            graphTime = 0;
+        }
         LFO_index = LFO_index + (float)modulatorSpeed;
         if (volume) 
         {
@@ -37,8 +46,8 @@ public class MusicController : MonoBehaviour
         }
         if (pitch)
         {
-            //ddddddaudioSource.pitch = (float)((Mathf.Sin(LFO_index) * modulatorDepth));
-            audioSource.pitch = (float)(LFOSawtoothWave(LFO_index));
+            //audioSource.pitch = (float)((Mathf.Sin(LFO_index) * modulatorDepth));
+            audioSource.pitch = Mathf.Abs((float)(LFOSawtoothWave(LFO_index)));
         }
         
     }
